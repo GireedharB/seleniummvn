41f47099ac98a130fcdd042b79b1feb963155e4a
GireedharB <gireedharbirajdar1011@gmail.com>
2024-12-08 14:04:46 +0530

    New repo updated

---
+++ src/test/java/test/Automation1.java
@@ -0,0 +1,19 @@
+
+/*Gireedhar*/
+
+package test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Automation1 {
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/login.php");
+		Thread.sleep(2000);
+		System.out.println(driver.getTitle());
+		System.out.println("Visited to Facebook website successfully");
+		driver.close();
+		}
+
+}
